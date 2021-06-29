80f0b95656b7b6283e3ac249d3d884b169cad284
qiang4030 <weiqiang4030@163.com>
2021-06-30 01:37:37 +0800

    完成用户列表功能的开发

--- src/main.js
+++ src/main.js
@@ -1,25 +1,23 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import './plugins/element.js'
-import './assets/css/global.css'
-import './assets/js/64d58efce2.js'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import "./plugins/element.js";
+import "./assets/css/global.css";
+import "./assets/js/64d58efce2.js";
 
-import axios from 'axios'
-axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1'
-Vue.prototype.$http = axios
+import axios from "axios";
+axios.defaults.baseURL = "http://127.0.0.1:8888/api/private/v1";
+axios.interceptors.request.use((config) => {
+  console.log(config);
+  config.headers.Authorization = window.sessionStorage.getItem('token');
+  return config;
+  // 最后必须return config
+});
+Vue.prototype.$http = axios;
 
-import { FormItem, Form, Input, Message } from 'element-ui'
-
-Vue.use(Form)
-Vue.use(FormItem)
-Vue.use(Input)
-// Vue.use(Message)
-Vue.prototype.$message = Message
-
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
   router,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,4 +1,28 @@
 import Vue from 'vue'
-import { Button } from 'element-ui'
+import { FormItem, Form, Input, Message, Container, Header, Aside, Main, Button, Menu, MenuItem, MenuItemGroup, Submenu, Breadcrumb, BreadcrumbItem, Card, Row, Col, Table, TableColumn, Switch, Tooltip, Pagination, Dialog, MessageBox } from 'element-ui'
 
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Input)
 Vue.use(Button)
+Vue.use(Container)
+Vue.use(Header)
+Vue.use(Aside)
+Vue.use(Main)
+Vue.use(Menu)
+Vue.use(MenuItem)
+Vue.use(MenuItemGroup)
+Vue.use(Submenu)
+Vue.use(Breadcrumb)
+Vue.use(BreadcrumbItem)
+Vue.use(Card)
+Vue.use(Row)
+Vue.use(Col)
+Vue.use(Table)
+Vue.use(TableColumn)
+Vue.use(Switch)
+Vue.use(Tooltip)
+Vue.use(Pagination)
+Vue.use(Dialog)
+Vue.prototype.$message = Message
+Vue.prototype.$confirm = MessageBox.confirm
